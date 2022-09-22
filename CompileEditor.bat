585b48f256bae708c43a9bd11103262686a88cb3
unknown <michael.bowen@mymail.champlain.edu>
2022-09-22 22:19:35 +0000

    Compiling editor to Development

--- CompileEditor.bat
+++ CompileEditor.bat
@@ -1,2 +1,2 @@
-Call "C:\Program Files\Epic Games\UE_5.0\Engine\Build\BatchFiles\Build.bat" %PROJECT_NAME%Editor Win64 Shipping "C:\Projects\%PROJECT_FOLDER%\%PROJECT_NAME%.uproject"
+Call "C:\Program Files\Epic Games\UE_5.0\Engine\Build\BatchFiles\Build.bat" %PROJECT_NAME%Editor Win64 Development "C:\Projects\%PROJECT_FOLDER%\%PROJECT_NAME%.uproject"
 EXIT /B %ERRORLEVEL%
