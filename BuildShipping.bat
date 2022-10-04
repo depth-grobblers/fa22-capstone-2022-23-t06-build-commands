4e80fd615d35371ab5b7403c580a98f5fdb2a056
Michael Bowen <michael@thebowenfamily.com>
2022-10-04 09:14:12 -0400

    Added Timer to buildShipping

--- BuildShipping.bat
+++ BuildShipping.bat
@@ -6,11 +6,16 @@ SET PROJECT_NAME=Dead_Pedal_2
 
 cd C:\Projects\%PROJECT_FOLDER%
 
+call :StartTimer
+
 git config --global --add safe.directory C:/Projects/%PROJECT_FOLDER%
 
 git pull origin main
 
 if NOT "%ERRORLEVEL%"=="0" EXIT /B %ERRORLEVEL%
+
+call :StopTimer
+call :DisplayTimerResult
 
 cd %WORKSPACE%
 
@@ -44,3 +49,26 @@ if %size% LSS 1000 (
 )
 
 EXIT /B 0
+
+:StartTimer
+:: Store start time
+set StartTIME=%TIME%
+for /f "usebackq tokens=1-4 delims=:., " %%f in (`echo %StartTIME: =0%`) do set /a Start100S=1%%f*360000+1%%g*6000+1%%h*100+1%%i-36610100
+goto :EOF
+
+:StopTimer
+:: Get the end time
+set StopTIME=%TIME%
+for /f "usebackq tokens=1-4 delims=:., " %%f in (`echo %StopTIME: =0%`) do set /a Stop100S=1%%f*360000+1%%g*6000+1%%h*100+1%%i-36610100
+:: Test midnight rollover. If so, add 1 day=8640000 1/100ths secs
+if %Stop100S% LSS %Start100S% set /a Stop100S+=8640000
+set /a TookTime=%Stop100S%-%Start100S%
+set TookTimePadded=0%TookTime%
+goto :EOF
+
+:DisplayTimerResult
+:: Show timer start/stop/delta
+echo Started: %StartTime%
+echo Stopped: %StopTime%
+echo Elapsed: %TookTime:~0,-2%.%TookTimePadded:~-2% seconds
+goto :EOF
